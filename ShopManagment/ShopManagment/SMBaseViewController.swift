166f9995fe5593e757f480c7d02d259a1b0f59c9
Mac <mac@Macs-MacBook-Air.local>
2018-07-22 00:03:25 +0530

    first push

---
+++ ShopManagment/ShopManagment/SMBaseViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SMBaseViewController.swift
+//  ShopManagment
+//
+//  Created by Mac on 21/07/18.
+//  Copyright © 2018 Mac. All rights reserved.
+//
+
+import UIKit
+
+class SMBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("hello")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
